bfaa175e9dc5d687f3d901d81abb2e9899283a90
Josue Sosa <60411213+userlab-josuesosa@users.noreply.github.com>
2022-10-12 20:14:20 -0600

    Loader fixed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Demo Data Visualization Project
 
+## To run locally
+
+- make sure you have node and yarn installed
+- run `yarn install` on the project root
+- run `yarn start`
--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,4 +1,8 @@
 const Spinner: React.FC<{}> = () => {
-  return <div className='loader' />
+  return (
+    <div className='loader-container'>
+      <div className='loader' />
+    </div>
+  )
 }
 export default Spinner
